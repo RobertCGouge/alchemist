3b707e48e506e48a5a2635e3167f64391a22764d
Gouge, Robert <robert.gouge@cgi.com>
2021-01-12 20:01:14 -0600

    first commit

---
+++ lesson1/guessing_game.ex
@@ -0,0 +1,29 @@
+defmodule GuessingGame do
+
+  def guess(a, b) when a > b, do: guess(b,a)
+
+  def guess(low, high) do
+    case String.trim(IO.gets("Hmm... maybe you're thinking of #{mid(low,high)}?\n}")) do
+      "bigger" -> bigger(low, high)
+      "smaller" -> smaller(low,high)
+      "yes" -> "I knew it!"
+      _ ->
+        IO.puts(~s(Please enter "bigger", "smaller", or "yes"))
+        guess(low, high)
+    end
+  end
+  def mid(low, high) do
+    div(low + high, 2)
+  end
+
+  def bigger(low, high) do
+    new_low = min(high, mid(low, high) + 1)
+    guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low, high) - 1)
+    guess(low, new_high)
+  end
+
+end
